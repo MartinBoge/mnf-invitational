1d2c6552ced3872776959709567c8cc957c977ac
Martin Bøge Jørgensen <martin.boge@live.dk>
2024-09-15 18:36:07 +0200

    feat: svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let title = 'Home';
+</script>
+
+<h1>{title} bithces</h1>
